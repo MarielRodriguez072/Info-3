6310866ebc2000120d3041d37ce931e8f64d78e3
MarielRodriguez072 <rodriguezmariel072@gmail.com>
2025-08-14 16:07:15 -0300

    agregue el archivo de gestion

---
+++ src/gestion_tareas.java
@@ -0,0 +1,22 @@
+/*Contexto:
+ Imagina que trabajas en una pequeña startup y te han pedido desarrollar un sistema simple en Java para gestionar tareas personales. El sistema debe permitir:
+Agregar tareas con una descripción y un estado (“pendiente” o “completada”).
+Listar todas las tareas.
+Marcar una tarea como completada.
+Eliminar tareas completadas.
+Requisitos Técnicos:
+Debe usarse una clase Tarea con atributos descripcion y estado.
+Una clase GestorTareas que almacene las tareas en un ArrayList<Tarea> y tenga métodos para cada funcionalidad.
+Un main que muestre un menú en consola para que el usuario pueda elegir qué hacer.
+Usar bucles (while, for) y condicionales (if, switch) para controlar el flujo.
+Validar que no se ingresen descripciones vacías.
+Extra (para puntos adicionales):
+Permitir que el usuario guarde las tareas en un archivo de texto y las cargue al iniciar el programa.
+*/
+
+public class gestion_tareas {
+
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+}
